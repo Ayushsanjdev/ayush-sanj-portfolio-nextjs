3d6e5f4162d65e7eccecd8ac4b7f9b0171cea53d
pardhme <mpradha9894@gmail.com>
2021-08-29 23:42:05 +0530

    new navbar ui

--- components/head.tsx
+++ components/head.tsx
@@ -1,38 +1,121 @@
-import Link from 'next/link'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
-import profile from '../public/profile.jpg'
-import { motion } from 'framer-motion'
+import Profile from '../public/profile.jpg'
+import Link from 'next/link'
+import Head from 'next/head'
 
-const Header = () => {
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'About', href: '/about', current: false },
+  { name: 'Projects', href: '/projects', current: false },
+  { name: 'Blogs', href: '/blogs', current: false },
+]
 
-  const navigation = [
-    { name: "Home", href: "/" },
-    { name: "About me", href: "/about" },
-    { name: "Projects", href: "/projects" },
-    { name: "Blogs", href: "/blogs" },
-  ];
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(' ')
+}
 
+const Header = () => { 
   return (
-    <header className="flex justify-between mt-4 px-4 py-4">
-        <Image
-          alt="my-picture"
-          src={profile}
-          width={50}
-          height={50}
-          className="rounded-full"
-        />
-        <nav className="md:block ml-10 pr-4 space-x-16">
-            {navigation.map((links) => (
-              <Link key={links.name} href={links.href}>
-              <a
-                key={links.name} href={links.href}
-                className="font-medium text-gray-500 hover:text-gray-900 align-middle"
-              >
-                {links.name}
-              </a></Link>
-            ))}
-        </nav>
-      </header>
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="block lg:hidden h-10 w-auto"
+                    src="https://img.icons8.com/clouds/100/000000/naruto-sign.png"
+                    alt="my leaf logo"
+                  />
+                  <img
+                    className="hidden lg:block h-10 w-auto"
+                    src="https://img.icons8.com/clouds/100/000000/naruto-sign.png"
+                    alt="my leaf logo"
+                  />
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <li key={item.name} className={classNames(
+                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'px-3 py-2 rounded-md text-sm font-medium' 
+                      )}>
+                        <Link
+                          // key={item.name}
+                          href={item.href}
+                          // className={classNames(
+                          //   item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          //   'px-3 py-2 rounded-md text-sm font-medium'
+                          // )}
+                          // aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* Profile dropdown */}
+                <Menu as="div" className="ml-3 relative">
+                  <div>
+                    <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">Open user menu</span>
+                      <Image className="rounded-full" width={50} height={50} src={Profile} alt="owner image" />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"></Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   )
 }
 
